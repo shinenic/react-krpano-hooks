e155b244d6acc4343f0214930cf115350cb2f82b
entin.wei <entin.wei@rakuten.com>
2020-09-19 23:54:50 +0800

    Updates

---
+++ precache-manifest.4dc40fb96fcc87db3121012f0e1b8df6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003da250941b776454416ee2c0e4622d",
+    "url": "/react-krpano-hooks/index.html"
+  },
+  {
+    "revision": "c4ebe952964b348cae0a",
+    "url": "/react-krpano-hooks/static/css/main.807c7422.chunk.css"
+  },
+  {
+    "revision": "34bb802b6365f9368efa",
+    "url": "/react-krpano-hooks/static/js/2.9157c7b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-krpano-hooks/static/js/2.9157c7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ebe952964b348cae0a",
+    "url": "/react-krpano-hooks/static/js/main.cdccb8c4.chunk.js"
+  },
+  {
+    "revision": "ffef94fc33de7693e434",
+    "url": "/react-krpano-hooks/static/js/runtime-main.9438f5ba.js"
+  }
+]);
